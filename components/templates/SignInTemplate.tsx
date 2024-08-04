3a93c78fb1356da2e1b4dcb92009c2d0729a5123
Truong <truongtdph40209@fpt.edu.vn>
2024-08-05 03:19:54 +0700

    Add stack and router for forgotpassword screen

--- components/templates/SignInTemplate.tsx
+++ components/templates/SignInTemplate.tsx
@@ -25,6 +25,7 @@ const SignInTemplate: React.FC = (): JSX.Element => {
   }
 
   const redirectToForgot = (): void => {
+    router.push('/authentication/Forgot')
   }
 
   const ButtonSignin = (): void => {
